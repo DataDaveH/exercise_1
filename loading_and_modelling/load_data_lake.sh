d4d3637973db7807b53aab2e2fb2e2d70ff30e58
w205 <w205@ip-172-31-15-152.ec2.internal>
2017-02-17 22:32:04 +0000

    data_lake script change

--- loading_and_modelling/load_data_lake.sh
+++ loading_and_modelling/load_data_lake.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+#!/bin/bash
 # 
 # prerequisite
 # 
 # all files have been downloaded, unzipped, and relevant csv files are local
-
+# 
 # remove headers and rename files for ease of reference
 mkdir data_to_load
-tail -n +2 "Hospital General Information.csv" > hospitals.csv
-tail -n +2 "Timely and Effective Care - Hospital.csv" > effective_care.csv
-tail -n +2 "Readmissions and Deaths - Hospital.csv" > readmissions.csv
-tail -n +2 "Measure Dates.csv" > measures.csv
-tail -n +2 "hvbp_hcahps_11_10_2016.csv" > survey_responses.csv
+tail -n +2 "Hospital General Information.csv" > ./data_to_load/hospitals.csv
+tail -n +2 "Timely and Effective Care - Hospital.csv" > ./data_to_load/effective_care.csv
+tail -n +2 "Readmissions and Deaths - Hospital.csv" > ./data_to_load/readmissions.csv
+tail -n +2 "Measure Dates.csv" > ./data_to_load/measures.csv
+tail -n +2 "hvbp_hcahps_11_10_2016.csv" > ./data_to_load/survey_responses.csv
 
-
-
-
+# now move all the files to HDFS
+hdfs dfs -mkdir /user/w205/hospital_compare
+hdfs dfs -put ./data_to_load/"hospitals.csv" /user/w205/hospital_compare
+hdfs dfs -put ./data_to_load/"effective_care.csv" /user/w205/hospital_compare
+hdfs dfs -put ./data_to_load/"readmissions.csv" /user/w205/hospital_compare
+hdfs dfs -put ./data_to_load/"measures.csv" /user/w205/hospital_compare
+hdfs dfs -put ./data_to_load/"survey_responses.csv" /user/w205/hospital_compare
